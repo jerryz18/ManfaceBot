195c45b727a90c0f2b97ab54d6c57dfa71288352
Jerry Zheng <91507126+jerryz18@users.noreply.github.com>
2021-12-07 04:29:22 +0000

    added yomama feature

---
+++ main.py
@@ -0,0 +1,27 @@
+import os
+import discord
+import random
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+  print("Hello {0.user}".format(client))
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+  
+  adjective = ["fat", "chunky", "poop4", "chungus", "amongus","scarfmon","obamacare","fat","ugly","short","skinny","unpoggers", "unbased","bluepilled","sponchating","genshinimpact playing"]
+
+  verb = ["poop her pants", "got kicked out of apple bees", "went to prison", "failed elementary school",
+  "walked under clock tower", "doctor pepper ", "played genshin impact", "did too much trolling",
+  "ate chungus juice", "walks her dog upside down", "uefduweudwe28du2882du82u8de2uued poop4", "poop4"]
+
+  if message.content.startswith("-yomama"):
+    await message.channel.send("Yo mama is so " + random.choice(adjective) + " she " + random.choice(verb) + "!")
+
+my_secret = os.environ['token']
+client.run(my_secret)
+
