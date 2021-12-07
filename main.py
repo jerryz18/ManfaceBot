6535ac421774887b54fce16cb1988efe0a1693ba
Gautham Nagrajan <74613485+GCity23@users.noreply.github.com>
2021-12-06 23:49:49 -0500

    Update main.py

--- main.py
+++ main.py
@@ -18,12 +18,17 @@ async def on_message(message):
   verb = ["poop her pants", "got kicked out of apple bees", "went to prison", "failed elementary school",
   "walked under clock tower", "doctor pepper ", "played genshin impact", "did too much trolling",
   "ate chungus juice", "walks her dog upside down", "uefduweudwe28du2882du82u8de2uued poop4", "poop4"]
+  
+  verb2 = ["poop his pants", "got kicked out of apple bees", "went to prison", "failed elementary school",
+  "walked under clock tower", "doctor pepper ", "played genshin impact", "did too much trolling",
+  "ate chungus juice", "walks his dog upside down", "uefduweudwe28du2882du82u8de2uued poop4", "poop4"]
 
   if message.content.startswith("-ym") or message.content.startswith("-yomama"):
     await message.channel.send("Yo mama is so " + random.choice(adjective) + " she " + random.choice(verb) + "!")
   if message.content.startswith("-yd") or message.content.startswith("-yodad"):
-    await message.channel.send("Yo dad is so " + random.choice(adjective) + " she " + random.choice(verb) + "!")
-
+    await message.channel.send("Yo dad is so " + random.choice(adjective) + " he " + random.choice(verb2) + "!")
+  if message.content.startswith("-dn") or message.content.startswith("-deeznuts"):
+    await message.channel.send("DEEZ NUTS IN YO MOUTH. GOTEEEEM")
 my_secret = os.environ['token']
 client.run(my_secret)
 
